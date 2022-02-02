c061dd81de7bbc8c56cc1c7ce63a8d62fa3ea18d
Gleisson Bezerra <gbezerra@microsoft.com>
2022-02-02 05:21:50 -0300

    Samples

---
+++ AULA02/correcao/device-to-cloud/iot_configs.h
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+// Wifi
+#define IOT_CONFIG_WIFI_SSID "GNET 2.4"
+#define IOT_CONFIG_WIFI_PASSWORD "F23700623D8D24"
+
+// Azure IoT
+#define IOT_CONFIG_IOTHUB_FQDN "homethings.azure-devices.net"
+#define IOT_CONFIG_DEVICE_ID "ESP-01"
+#define IOT_CONFIG_DEVICE_KEY "m9rIJ5ZQ4legSv4h2WR/Su0BQiq/VtPCE1Oenf979iU="
+
+// Publish 1 message every 2 seconds
+#define TELEMETRY_FREQUENCY_MILLISECS 2000
